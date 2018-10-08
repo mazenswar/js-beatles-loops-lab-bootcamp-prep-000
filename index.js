be6188ba67d1949416fcd7c7f1d9af278fcd35f6
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 22:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // add solution here
 function theBeatlesPlay(artist, instrument) {
   var arr=[];
-  for (let i=0; i < artist.length; i++) {
+
+}
+
+
+
+  /*for (let i=0; i < artist.length; i++) {
     for (let x=0; x < instrument.length; x++) {
       arr.push(`${artist} plays ${instrument[x]}`);
     }
-  }
-}
+  }*/
