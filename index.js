b2ab3928c3122266ab0de3101d2f749814ea9b12
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 23:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ function johnLennonFacts(arr) {
 
 function iLoveTheBeatles(num) {
   let arr=[];
-  do {arr.push("I love the Beatles!")}
+  do {arr.push("I love the Beatles!")
+    num++;
+  }
   while(num < 15);
+  return arr;
 }
