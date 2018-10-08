e5d2ebb1f6a1e5fafddff0af3d2b9b62a8ca4978
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 22:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(artist, instrument) {
   var arr=[];
   for (let i=0; i < artist.length; i++) {
     for (let x=0; x < instrument.length; x++) {
-      arr.push(`${artist[i]} plays ${instrument[x]}`);
+      arr.push(`${artist} plays ${instrument[x]}`);
     }
   }
 }
